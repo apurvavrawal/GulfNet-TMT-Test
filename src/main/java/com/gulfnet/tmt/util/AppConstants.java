444bd956b243b6b40f6c24c9f9ce8261bab3e8ef
RJ2610 <ranu.jain@joshsoftware.com>
2023-12-01 15:09:08 +0530

    Correct the Admin roles

--- src/main/java/com/gulfnet/tmt/util/AppConstants.java
+++ src/main/java/com/gulfnet/tmt/util/AppConstants.java
@@ -6,7 +6,7 @@ public final class AppConstants {
 
     public static String HQ_GROUP_CODE = "HQ";
     public static final List<String> APP_TYPE_MOBILE = List.of("ADMIN", "MOBILE");
-    public static final List<String> ADMIN_USER_ROLE_VALUE = List.of("ADMIN", "MOBILE", "HQ", "DELETE");
+    public static final List<String> ADMIN_USER_ROLE_VALUE = List.of("ADMIN");
     public static final List<String> MOBILE_USER_ROLE_VALUE = List.of("MOBILE", "HQ", "DELETE");
 
 
