199cad5f11b58adffebd992230d56adbd037a566
Ravi Rathore <ravikumar.rathore@signzy.com>
2023-12-21 16:25:32 +0530

    bugfix | GTMT-154 | removed phone and dorakuUserCode valiation

--- src/main/java/com/gulfnet/tmt/validator/UserValidator.java
+++ src/main/java/com/gulfnet/tmt/validator/UserValidator.java
@@ -96,13 +96,13 @@ public final class UserValidator {
                     MessageFormat.format(ErrorConstants.NOT_VALID_ERROR_MESSAGE, "Email")));
         }
 
-        if (StringUtils.isEmpty(user.getPhone())){
-            errors.add(new ErrorDto(ErrorConstants.MANDATORY_ERROR_CODE,
-                    MessageFormat.format(ErrorConstants.MANDATORY_ERROR_MESSAGE, "Phone")));
-        } else if (!Pattern.matches(gulfNetTMTServiceConfig.getRegExPhone(), user.getPhone())) {
-            errors.add(new ErrorDto(ErrorConstants.NOT_VALID_ERROR_CODE,
-                    MessageFormat.format(ErrorConstants.NOT_VALID_ERROR_MESSAGE, "Phone")));
-        }
+//        if (StringUtils.isEmpty(user.getPhone())){
+//            errors.add(new ErrorDto(ErrorConstants.MANDATORY_ERROR_CODE,
+//                    MessageFormat.format(ErrorConstants.MANDATORY_ERROR_MESSAGE, "Phone")));
+//        } else if (!Pattern.matches(gulfNetTMTServiceConfig.getRegExPhone(), user.getPhone())) {
+//            errors.add(new ErrorDto(ErrorConstants.NOT_VALID_ERROR_CODE,
+//                    MessageFormat.format(ErrorConstants.NOT_VALID_ERROR_MESSAGE, "Phone")));
+//        }
 
         if (!APP_TYPE_MOBILE.contains(user.getAppType())) {
             errors.add(new ErrorDto(ErrorConstants.NOT_VALID_ERROR_CODE,
@@ -117,10 +117,10 @@ public final class UserValidator {
                     MessageFormat.format(ErrorConstants.NOT_VALID_ERROR_MESSAGE, user.getLanguagePreference() + " language ")));
         }
 
-        if (APP_TYPE_MOBILE.get(1).equalsIgnoreCase(user.getAppType()) && gulfNetTMTServiceConfig.isDorakuConfigure() && StringUtils.isEmpty(user.getDorakuUserCode())) {
-            errors.add(new ErrorDto(ErrorConstants.MANDATORY_ERROR_CODE,
-                    MessageFormat.format(ErrorConstants.MANDATORY_ERROR_MESSAGE, "Doraku User Code")));
-        }
+//        if (APP_TYPE_MOBILE.get(1).equalsIgnoreCase(user.getAppType()) && gulfNetTMTServiceConfig.isDorakuConfigure() && StringUtils.isEmpty(user.getDorakuUserCode())) {
+//            errors.add(new ErrorDto(ErrorConstants.MANDATORY_ERROR_CODE,
+//                    MessageFormat.format(ErrorConstants.MANDATORY_ERROR_MESSAGE, "Doraku User Code")));
+//        }
 
         if (CollectionUtils.isEmpty(user.getUserRole())) {
             errors.add(new ErrorDto(ErrorConstants.MANDATORY_ERROR_CODE,
